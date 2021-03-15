6995cbac432320dea8c7d241645dbab06be9b12d
alexzamurca <alexzamurca@yahoo.co.uk>
2021-03-15 00:06:53 +0000

    added fake water to scene - looks like fish is jumping from water and player dies appropriately. When jump on fish, player gets knockup to help get back on boat (basic fish jumping is done).

--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -9,6 +9,7 @@ public class FishMovement : MonoBehaviour
     private BoxCollider2D boxCollider2D;
     private bool isJumping = false;
     [SerializeField] private float jumpVelocity = 100f;
+    [SerializeField] private float playerUpwardsPush = 10f;
 
     void Awake()
     {
@@ -29,5 +30,18 @@ public class FishMovement : MonoBehaviour
         RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f,
             Vector2.down, .1f, waterLayerMask);
         return raycastHit2D.collider != null;
+    }
+
+    // If we collide with a player, send the player jumping up
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        GameObject colliderGameObject = other.gameObject;
+        if (colliderGameObject.CompareTag("Player"))
+        {
+            // Get player rb
+            Rigidbody2D playerRb = colliderGameObject.GetComponent<Rigidbody2D>();
+            // Push the player up
+            playerRb.velocity = Vector2.up * playerUpwardsPush;
+        }
     }
 }
